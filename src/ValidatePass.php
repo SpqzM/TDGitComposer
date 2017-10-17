df41a14e66b6cf60b9cd3bfed05511a0a49ce318
SpqzM <nico.davy@gmail.com>
2017-10-17 09:56:00 +0200

    validate

--- src/ValidatePass.php
+++ src/ValidatePass.php
@@ -12,7 +12,7 @@
  * @author PC101
  */
 class ValidatePass {
-    const MINXLGT = 6;
+    const MINLGT = 6;
     const MAXLGT = 20;
     public function ValidLength($pass){
         $passlength = strlen($pass);
